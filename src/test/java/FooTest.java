49cd88b37448249676a3ae444ef0cc3880728fe5
Christian Garbs <mitch@cgarbs.de>
2017-06-02 23:16:42 +0200

    use Foo class instead of stray .gitignore file to populate src/main/java directory structure

--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -3,10 +3,12 @@ import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
-public class FooTest {
+public class FooTest
+{
 
 	@Test
-	public void exampleTest() {
-		assertThat("foo", is("foo"));
+	public void barOnFooYieldsFoobar()
+	{
+		assertThat(new Foo().bar(), is("foobar"));
 	}
 }
